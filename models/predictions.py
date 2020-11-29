507ebbdf126e8bf117db9aae04ef1bdc7a3d441b
Brian Hu <bhurobin221@gmail.com>
2020-11-29 18:48:18 -0500

    updated predictions

--- models/predictions.py
+++ models/predictions.py
@@ -20,6 +20,8 @@ for file in pca_tf_models:
     rmse.update_state(y_pred, labels_test)
     print(file + ": " + str(rmse.result().numpy()))
     rmse.reset_states()
+    y_pred = pd.DataFrame(y_pred)
+    y_pred.to_hdf(file.split(".")[0] + "_pred.h5", "data", mode="w", format="f")
 
 for file in ae_tf_models:
     model = tf.keras.models.load_model(file)
@@ -27,6 +29,8 @@ for file in ae_tf_models:
     rmse.update_state(y_pred, labels_test)
     print(file + ": " + str(rmse.result().numpy()))
     rmse.reset_states()
+    y_pred = pd.DataFrame(y_pred)
+    y_pred.to_hdf(file.split(".")[0] + "_pred.h5", "data", mode="w", format="f")
 
 for file in pca_sk_models:
     model = joblib.load(file)
@@ -34,3 +38,14 @@ for file in pca_sk_models:
     rmse.update_state(y_pred, labels_test)
     print(file + ": " + str(rmse.result().numpy()))
     rmse.reset_states()
+    y_pred = pd.DataFrame(y_pred)
+    y_pred.to_hdf(file.split(".")[0] + "_pred.h5", "data", mode="w", format="f")
+
+for file in ae_sk_models:
+    model = joblib.load(file)
+    y_pred = model.predict(ae_test)
+    rmse.update_state(y_pred, labels_test)
+    print(file + ": " + str(rmse.result().numpy()))
+    rmse.reset_states()
+    y_pred = pd.DataFrame(y_pred)
+    y_pred.to_hdf(file.split(".")[0] + "_pred.h5", "data", mode="w", format="f")
