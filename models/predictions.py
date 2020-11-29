c9ddaa4932b702d02311d0d12d30cc81a0d38f09
Brian Hu <bhurobin221@gmail.com>
2020-11-29 17:28:03 -0500

    uploaded prediction script

---
+++ models/predictions.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import pandas as pd
+import joblib
+import tensorflow as tf 
+
+pca_tf_models = []
+pca_sk_models = []
+ae_tf_models = []
+ae_sk_models = []
+
+pca_test = pd.read_hdf('').values
+ae_test = pd.read_hdf('').values 
+labels_test = pd.read_hdf('').values
+
+rmse = tf.metrics.RootMeanSquaredError()
+
+for file in pca_tf_models:
+    model = tf.keras.models.load_model(file)
+    y_pred = model.predict(pca_test)
+    rmse.update_state(y_pred, labels_test)
+    print(file + ": " + str(rmse.result().numpy()))
+    rmse.reset_states()
+
+for file in ae_tf_models:
+    model = tf.keras.models.load_model(file)
+    y_pred = model.predict(ae_test)
+    rmse.update_state(y_pred, labels_test)
+    print(file + ": " + str(rmse.result().numpy()))
+    rmse.reset_states()
+
+for file in pca_sk_models:
+    model = joblib.load(file)
+    y_pred = model.predict(pca_test)
+    rmse.update_state(y_pred, labels_test)
+    print(file + ": " + str(rmse.result().numpy()))
+    rmse.reset_states()
